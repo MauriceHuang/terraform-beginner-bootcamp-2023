615d18b7c95d452b6df11fe21d0bb09d993c6aea
Maurice Huang <Maurice.huang.pai.so@gmail.com>
2023-10-13 09:01:42 +0800

    44 terratowns provider (#48)
    * #44 wip setting up the skeleton for creating a custom terraform provider
    
    * #44 should be able to run terraform provider block in terraform
    
    * #44 set up skeleton for resources
    
    * wip
    
    * #44 finish implementing tf provider

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terratowns= {
 }
 }
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
@@ -22,13 +22,14 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
-resource "terratown_home" "home" {
-  name= "how to win in Warcraft 3"
+
+resource "terratowns_home" "home" {
+  name = "How to win at warcraft 3 ROC"
   description = <<DESCRIPTION
-Warcraft III is a real-time strategy game developed by Blizzard Entertainment. It was released in 2002 and quickly became a classic in the genre. The game combines intricate strategy, resource management, and tactical combat set in the world of Azeroth. Players can choose to command one of four factions, each with unique units and abilities, and engage in single-player campaigns or multiplayer battles. Warcraft III is known for its engaging storytelling, memorable characters, and the introduction of the "Hero" unit concept, which greatly influenced the genre. It also features a powerful map editor that enabled the creation of custom games, leading to the birth of the popular multiplayer online battle arena (MOBA) genre.
-  DESCRIPTION
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "abcd1234.cloudfront.net"
+Warcraft III is a real-time strategy game developed by Blizzard Entertainment. It was released in 2002 and is set in the high fantasy universe of Warcraft. The game features four playable factions: Humans, Orcs, Undead, and Night Elves, each with unique units and abilities. Players must gather resources, build bases, and command armies to defeat their opponents. Warcraft III introduced several innovative features, including hero units with RPG-like progression and a powerful map editor that spawned the popular custom game scene. The game is highly regarded for its deep gameplay, compelling story, and influential impact on the strategy genre.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "abc123.cloudfront.net"
   town = "gamers-grotto"
   content_version = 1
 }
